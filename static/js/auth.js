92062f0d941838bcc432b8324dd12c5c39d9e4fd
Gioele-M <gioelemontis97@gmail.com>
2022-06-06 00:10:55 +0100

    mini-breakdown of layout.js + minor changes in auth.js

--- static/js/auth.js
+++ static/js/auth.js
@@ -6,7 +6,7 @@ async function requestLogin(e){
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(Object.fromEntries(new FormData(e.target))) //Double check that this object is actually fine with backend (main thing is check together with Billie that all the 'name' attributes of the form data matches w backend parameters)
         }
-        const r = await fetch(`http://localhost:3000/auth/login`, options) // Login endpoint needs to be changed
+        const r = await fetch(`http://localhost:3000/auth/login`, options) // Login endpoint needs to be checked
         const data = await r.json()
         if (data.err){ throw Error(data.err); }
         login(data);
@@ -23,7 +23,7 @@ async function requestRegistration(e) {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(Object.fromEntries(new FormData(e.target))) // same as before, doublecheck w backend
         }
-        const r = await fetch(`http://localhost:3000/auth/register`, options) 
+        const r = await fetch(`http://localhost:3000/auth/register`, options) // endpoint needs to be checked 
         const data = await r.json()
         if (data.err){ throw Error(data.err) }
         requestLogin(e);
