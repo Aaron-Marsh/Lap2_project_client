b34925ca1201a5b91d062d3eacd4f1b5f875ca14
Gioele-M <gioelemontis97@gmail.com>
2022-06-06 00:06:04 +0100

    not-so-useful pseudocode for content.js

---
+++ static/js/content.js
@@ -0,0 +1,3 @@
+// This part of JS contains functions to render the page content
+
+// We have to decide with Billie if we want to do this creating elements and looping through them in JS or if we want to use EJS and work from the HTML. 
