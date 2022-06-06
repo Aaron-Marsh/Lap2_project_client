f5f68f69cffb0fddf4530703c9fa9267d8293bee
Gioele-M <gioelemontis97@gmail.com>
2022-06-06 16:52:46 +0100

    rendering works with event listeners

--- static/js/content.js
+++ static/js/content.js
@@ -7,39 +7,6 @@
 const divToAppend = document.querySelector('#divToAppend')
 
 
-
-
-// <div class="btn-group">
-//                 <button class="btn btn-primary text-white mt-3 border-0 " type="button" style="font-size: 25px;" id="minusBtn">-</button>
-//                 <button class="btn btn-primary text-white mt-3 border-0 w-100" type="button" style="font-size: 25px;" data-toggle="modal" data-target="#trackHabit">5 a day</button>
-//                 <button class="btn btn-primary text-white mt-3 border-0" type="button" style="font-size: 25px;" id="plusBtn">+</button>
-//                 </div>
-
-//                 <button class="btn-lg btn-primary text-white mt-3 rounded-pill border-0 w-100 " type="button" data-toggle="modal" data-target="#exampleModalCenter" style="font-size: 25px;">Add habit!</button>
-
-
-
-
-
-function renderLoginForm() {
-    const fields = [
-        { tag: 'input', attributes: { type: 'email', name: 'email', placeholder: 'Email' } },
-        { tag: 'input', attributes: { type: 'password', name: 'password', placeholder: 'Password' } },
-        { tag: 'input', attributes: { type: 'submit', value: 'Login' } }
-    ]
-    const form = document.createElement('form');
-    fields.forEach(f => {
-        let field = document.createElement(f.tag);
-        Object.entries(f.attributes).forEach(([a, v]) => {
-            field.setAttribute(a, v);
-            form.appendChild(field);
-        })
-    })
-    form.addEventListener('submit', requestLogin)
-    main.appendChild(form);
-}
-
-
 function createDivHabit(habitText, habitID){
     const fields = [
         {tag: 'button', value: '-',attributes: {type: 'button', style:'font-size: 25px;', class: 'btn btn-primary text-white mt-3 border-0'}},
@@ -56,35 +23,80 @@ function createDivHabit(habitText, habitID){
         Object.entries(f.attributes).forEach(([a, v])=>{
             field.setAttribute(a, v)
 
-            //add the event listener of interest!!!
-            
+             //event listener
+
             mainDiv.appendChild(field)
         })
 
         field.textContent = f.value
-
-        // REMEMBER TO ADD THE EVENT LISTENER!!!!!
-
-        console.log(field)
-
-        
+    
+        // switch statement to add event listener based on f.value
+        switch(f.value){
+            case '-':
+                field.addEventListener('click', e=>{
+                    minusClicked(e, habitID)
+                })
+                break;
+            case '+':
+                field.addEventListener('click', e=>{
+                    plusClicked(e, habitID)
+                })
+                break;
+            default:
+                field.addEventListener('click', e=>{
+                    mainClicked(e, habitID)
+                })
+        }        
     })
 
     divToAppend.appendChild(mainDiv)
-    
 
 }
 
 
-createDivHabit('this is some sample text', 'sorry its boring')
-createDivHabit('this is some sample text', 'sorry its boring')
-createDivHabit('this is some sample text', 'sorry its boring')
+createDivHabit('this is some sample text', 'ID 1')
+createDivHabit('this is some sample text', 'ID 2')
+createDivHabit('this is some sample text', 'ID 3')
 
 
 
 
-function minusWasClicked(habitID){
+function minusClicked(e, habitID){
+    e.preventDefault()
     // fetch...
     // send in the request the habitID, letting it know that the - button was clicked so the backend can update this info
+    console.log('minus was clicked with habit id: ' + habitID)
+}
+
+function plusClicked(e, habitID){
+    e.preventDefault()
+    console.log('plus was clicked with habit id: ' + habitID)
+    //
+}
+
+
+function mainClicked(e, habitID){
+    e.preventDefault()
+    console.log('main was clicked with habit id: ' + habitID)
+}
+
+
+
+
+//Modal(?)
+function getStreak(habitID){
 
 }
+
+function deleteHabit(habitID){
+
+}
+
+
+function addHabit(habitID){
+
+
+}
+
+
+
