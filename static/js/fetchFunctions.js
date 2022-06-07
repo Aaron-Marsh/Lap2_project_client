719a79b26487fa10aeb2b42618bed9f270e6bc49
Gioele-M <gioelemontis97@gmail.com>
2022-06-07 13:59:48 +0100

    get all posts by user fetch works

--- static/js/fetchFunctions.js
+++ static/js/fetchFunctions.js
@@ -1,12 +1,20 @@
+
+const heroku_url = 'https://glacial-plains-13166.herokuapp.com'
+
 async function getHabitsByUser(userId){
+    console.log('function is called')
     try{
-        let url = `${heroku_url}/user/${userId}`
+        let url = `${heroku_url}/habits/user/${userId}`
         const response = await fetch(url)
-        const data = await response.json()
-        console.log(data)
+        const data = await response.text()
+        console.log('this is the fetched data ' +data)
         return data
 
     }catch(err){
-        console.log({message: err.message})
+        return({message: err.message})
     }
 }
+
+const dataAaron = getHabitsByUser('Aaron')
+
+console.log(dataAaron)
