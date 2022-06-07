2110c4b8f29f2f58983b6b5a62fccce121437363
Tom Hughes <tomhughes1987@gmail.com>
2022-06-07 15:35:58 +0200

    add some client tests

--- test/index.test.js
+++ test/index.test.js
@@ -37,3 +37,53 @@ it('adds the input value to the h1', ()=>{
 	expect(h1.innerHTML).toBe(input.value)
 
 })
+
+
+// it('Test click event', () => {
+//     const mockCallBack = jest.fn();
+//     const button = shallow((<Button onClick={mockCallBack}>Ok!</Button>));
+//     button.find('button').simulate('click');
+//     expect(mockCallBack.mock.calls.length).toEqual(1); // check if it has been called once
+//   });
+
+
+
+
+
+
+///////////////NEW TESTS:
+
+//check the heading is Rabbit Habit!
+it('heading is correct', ()=>{
+	const h1 = document.querySelector('h1')
+	expect(h1.innerHTML).toBe("Rabbit Habits!")
+})
+
+//check the title is correct
+it('title is correct', ()=>{
+	const title = document.querySelector('title')
+	expect(title.innerHTML).toBe("Rabbit Habits")
+})
+ 
+//////////////////// NOT WORKING
+//check the favicon is correct
+it('NOT WORKING:favicon script is not blank', ()=>{
+	const favicon = document.querySelector('head')
+	expect(favicon).toContain('<link rel="icon" type="image/x-icon" href="imgs\favicon.ico">')
+})
+
+///////////////////check the logo NOT WORKING
+it('NOT WORKING: logo is correct', ()=>{
+	const logo = document.querySelector('rabbit_logo')
+	console.log(logo)
+	expect(logo.src).toBeMatch('./imgs/logo.png')
+})
+
+
+//BUTTON.type =btn
+it ('buttons types are button', ()=>{
+const button = document.querySelector("button")
+expect(button.type).toBe("button")
+})
+
+
